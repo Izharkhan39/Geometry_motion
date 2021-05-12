34920aa9d028e136815602bbf0b9506c2c236ee7
Izharkhan39 <80109303+Izharkhan39@users.noreply.github.com>
2021-05-12 12:42:15 +0530

    Update playermovement.cs

--- scripts/playermovement.cs
+++ scripts/playermovement.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
-{
+{   //public rigibody is ref in unity and rb i name
+    // forwarF & SideF is instances 
     public Rigidbody rb;
     public  float forwardforce =2000f;
     public float sidewayforce = 500f;
@@ -11,10 +12,12 @@ public class playermovement : MonoBehaviour
     void Update()
     {
         rb.AddForce(0, 0, forwardforce * Time.deltaTime);
-
+        //this code is for constant for in z direc
+        //TIme.deltaTime if framerate 
+        
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewayforce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);   
+            rb.AddForce(sidewayforce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);//forcemode.velocitychange adds a instant velocity change,ignoring its mass   
         }
         if (Input.GetKey("a"))
         {
